b607587c2b6c25b62b562f50841489d52d85cff7
Serlin Tamilselvam <t.serlin94@gmail.com>
2021-01-31 19:28:41 -0500

    UI: Serviceworker.js file path change

--- portfolio/src/serviceWorker.js
+++ portfolio/src/serviceWorker.js
@@ -58,7 +58,7 @@ export function register(config) {
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
-    .register(swUrl)
+    .register(swUrl, { scope: '/' })
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
