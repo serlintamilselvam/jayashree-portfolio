d5a91996471ad04fa52f070ce43a80c3048af198
Serlin Tamilselvam <t.serlin94@gmail.com>
2021-03-09 18:59:50 -0500

    UI: Added developer courtesy in footer

--- portfolio/src/Components/Footer/Footer.js
+++ portfolio/src/Components/Footer/Footer.js
@@ -5,24 +5,29 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div className="jay-footer" id="contact">
-            <Container fluid="lg">
-            <Row>
-                <Col xs={4} md={4} className="contact-text">
-                    Contact
-                </Col>
-                <Col xs={4} md={4}>
-                    <a className="open-mail" href="mailto:jayashreesrinivasan12@gmail.com"><FontAwesomeIcon icon="envelope" size="3x"/></a>
-                </Col>
-                <Col xs={4} md={4}>
-                    <a href="https://www.linkedin.com/in/jayashree-srinivasan-40aa7a179/?originalSubdomain=ca" 
-                    target="_blank"
-                    rel="noreferrer">
-                        <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x"/>
-                    </a>
-                </Col>
-            </Row> 
-            </Container>
+        <div className="footer-wrapper">
+            <div className="jay-footer" id="contact">
+                <Container fluid="lg">
+                    <Row>
+                        <Col xs={4} md={4} className="contact-text">
+                            Contact
+                        </Col>
+                        <Col xs={4} md={4}>
+                            <a className="open-mail" href="mailto:jayashreesrinivasan12@gmail.com"><FontAwesomeIcon icon="envelope" size="3x"/></a>
+                        </Col>
+                        <Col xs={4} md={4}>
+                            <a href="https://www.linkedin.com/in/jayashree-srinivasan-40aa7a179/?originalSubdomain=ca" 
+                            target="_blank"
+                            rel="noreferrer">
+                                <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x"/>
+                            </a>
+                        </Col>
+                    </Row> 
+                </Container>
+            </div>
+            <div className="developer-credits">
+                <b>Developed By: </b><a href="https://ca.linkedin.com/in/serlin-tamilselvam-240a12133">Serlin Tamilselvam</a>
+            </div>
         </div>
     )
 }
