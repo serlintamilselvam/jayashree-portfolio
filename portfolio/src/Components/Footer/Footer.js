e8c332e620f31d99ee270e5f7de5f9c10687cb92
Serlin Tamilselvam <t.serlin94@gmail.com>
2021-04-30 21:49:05 -0400

    Added dark and light theme button

--- portfolio/src/Components/Footer/Footer.js
+++ portfolio/src/Components/Footer/Footer.js
@@ -2,9 +2,14 @@ import React from 'react'
 import './Footer.scss'
 
 import { Container, Row, Col, Button } from 'react-bootstrap'
+import darkModeBtn from '../../Assets/Images/dark_mode_btn.png'
 
 
 class Footer extends React.Component {
+
+    changeWebsiteMode() {
+        alert("Oops! Sorry We are still working on light theme implementation.")
+    }
 
     render() {
         return (
@@ -15,6 +20,9 @@ class Footer extends React.Component {
                             <Button variant="danger" className="download-prof">Download profile</Button>
                         </Col>
                         <Col xs={6} md={6} className="toggle-bar">
+                            <img 
+                                src={darkModeBtn} alt="Dark mode enabled"
+                                onClick={this.changeWebsiteMode.bind(this)} />
                         </Col>
                     </Row> 
                 </Container>
