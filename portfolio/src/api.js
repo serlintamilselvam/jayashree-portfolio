5c2e57247594fb52f1f726db73ae7c84181e12f9
Serlin Tamilselvam <t.serlin94@gmail.com>
2021-05-23 13:15:06 -0400

    Produ url changed

--- portfolio/src/api.js
+++ portfolio/src/api.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const APISettings = {
-    backEndUrl: (process.env.NODE_ENV === 'production') ? 'https://trigger-email-service.azurewebsites.net' : process.env.REACT_APP_BACKEND_URL,
+    backEndUrl: (process.env.NODE_ENV === 'production') ? 'https://trigger-email-service.azurewebsites.net/' : process.env.REACT_APP_BACKEND_URL,
     adminEmailAddress: (process.env.NODE_ENV === 'production') ? 'jayashreesrinivasan12@gmail.com' : process.env.REACT_APP_ADMIN_EMAIL_ADDRESS,
     email: {
         sendToAdmin: 'email/send/info'
